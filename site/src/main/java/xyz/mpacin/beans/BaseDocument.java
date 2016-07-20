c2efe8b5991f945386d7982da05b03c6662f3648
mpacin <mpacin5@gmail.com>
2016-07-20 12:13:41 +0200

    First Commit
    Code generated by Maven artifact

---
+++ site/src/main/java/xyz/mpacin/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package xyz.mpacin.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="mpacinhippo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
